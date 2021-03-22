bbc25f08a01ba5c5223e0e48fde552fb91827cc8
SAMAKE MOHAMED <39623776+youzerssif@users.noreply.github.com>
2021-03-22 16:32:40 +0100

    passer les data avec state

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -3,10 +3,15 @@ import './App.css';
 import Table from './Table'
 import { Component } from 'react';
 
-class App extends Component {
-  render() {
 
-    const characters =[
+
+
+
+
+class App extends Component {
+
+  state = {
+    characters: [
       {
         name: 'Charlie',
         job: 'Janitor',
@@ -23,14 +28,28 @@ class App extends Component {
         name: 'Dennis',
         job: 'Bartender',
       },
-    ]
+    ],
+  }
+  removeCharacter = (index) => {
+    const {characters} = this.state
+  
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index
+      }),
+    })
+  }
+  
+  render() {
+
+    const { characters } = this.state
 
     return (
       <div className="App">
         <h1>Hello, React!</h1>
         <div className="container">
-        <Table characterData={characters} />
-      </div>
+          <Table characterData={characters} removeCharacter={this.removeCharacter} />
+        </div>
       </div>
       
     )
--- react-tutorial/src/Table.js
+++ react-tutorial/src/Table.js
@@ -15,29 +15,62 @@ const TableHeader = () =>{
 
 const TableBody = (props) =>{
 
-  const name = "Mohamed"
   const rows = props.characterData.map((row, index) =>{
     return (
       <tr key={index}>
-        <td>{row.name} </td>
+        <td>{row.name}</td>
         <td>{row.job}</td>
+        <td>
+          <button onClick={() => props.removeCharacter(index)}>Delete</button>
+        </td>
       </tr>
     )
   })
 
   return <tbody>{rows}</tbody>
   }
+ 
+
+  const Table = (props) => {
+    const {characterData, removeCharacter} = props
   
-class Table extends Component {
-  render() {
-    const {characterData} = this.props
     return (
       <table>
         <TableHeader />
-        <TableBody characterData={characterData} />
+        <TableBody characterData={characterData} removeCharacter={removeCharacter} />
       </table>
     )
   }
-}
+
+// class Table extends Component {
+//   render() {
+    
+//     // const {characterData} = this.props
+//     const characters =[
+//       {
+//         name: 'Charlie',
+//         job: 'Janitor',
+//       },
+//       {
+//         name: 'Mac',
+//         job: 'Bouncer',
+//       },
+//       {
+//         name: 'Dee',
+//         job: 'Aspring actress',
+//       },
+//       {
+//         name: 'Dennis',
+//         job: 'Bartender',
+//       },
+//     ]
+//     return (
+//       <table>
+//         <TableHeader />
+//         <TableBody characterData={characters} />
+//       </table>
+//     )
+//   }
+// }
 
 export default Table
